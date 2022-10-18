c760b6c456ef2b0fc0a075bbfb87ad38ca0aff67
elismar <infotec.elismar@gmail.com>
2022-10-18 19:35:29 -0300

    ola

---
+++ src/projeto01/proj.java
@@ -0,0 +1,7 @@
+package projeto01;
+
+public class proj {
+    public static void main(String[] args) {
+        System.out.println("olá World");
+    }
+}
--- src/projeto01/proj01.java
+++
@@ -1,5 +0,0 @@
-public class proj01 {
-    public static void main(String[] args) {
-        System.out.println("Olá World");
-    }
-}
